0ec6a8cb35284c5678f71a0ec0f7099eb8a87177
BeaumerF <francois.beaumer@epitech.eu>
2018-05-26 16:42:07 +0200

    first

---
+++ OpusMundi/DashBoard.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace OpusMundi
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DashBoard : Page
+    {
+        String userName;
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var param = e.Parameter as string;
+            userName = param;
+            UserName.Text = userName;
+            UserName2.Text =  " " + userName;
+        }
+
+        public DashBoard()
+        {
+            this.InitializeComponent();
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            //register back request event for current view
+            SystemNavigationManager.GetForCurrentView().BackRequested += BackRequested;
+        }
+
+        private void BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+            SystemNavigationManager.GetForCurrentView().BackRequested -= BackRequested;
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack();
+                e.Handled = true;
+            }
+        }
+
+        private void Border_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().BackRequested -= BackRequested;
+            Frame.Navigate(typeof(Meeting));
+        }
+
+        private void TextBlock_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+            SystemNavigationManager.GetForCurrentView().BackRequested -= BackRequested;
+            Frame.Navigate(typeof(Workspace), userName);
+        }
+    }
+}
